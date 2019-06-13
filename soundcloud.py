9a9009fba447b978a6d08e6d7e6ea84a937f16f3
Rourke L <rourke.lowe.eng@gmail.com>
2019-06-13 13:26:25 -0700

    initial attempt - going to try through api

--- soundcloud.py
+++ soundcloud.py
@@ -1,1 +1,24 @@
 #soundcloud new music
+
+#seems like it needs to be javascript?
+import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+
+
+allsongs = []
+soundcloudbaseurl = requests.get('https://soundcloud.com/you/likes')
+soundcloudbs = BeautifulSoup(soundcloudbaseurl.text, 'lxml')
+
+songtiles = soundcloudbs.find_all('li', attrs = {'class':'badgeList__item'} )
+
+for song in songtiles:
+    singlesong={}
+    songName = song.find('a', attrs = {'class':'playableTile__mainHeading'})
+
+    singlesong[songName] = songName.find().text.encode('utf-8')
+    allsongs.append(singlesong)
+
+print(allsongs)
+print(soundcloudbs)
--- youtube.py
+++ youtube.py
@@ -1,1 +1,23 @@
 #youtube new music
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+
+
+allsongs = []
+youtubebaseurl = requests.get('https://www.youtube.com/playlist?list=PL2BBFi7UvppsE_8WDMYRa9zFoh2LlET3Z')
+youtubebs = BeautifulSoup(youtubebaseurl.text, 'lxml')
+
+songtiles = youtubebs.find_all('ytd-playlist-video-renderer', attrs = {'class':'ytd-playlist-video'} )
+
+for song in songtiles:
+    singlesong={}
+    songName = song.find('h3', attrs = {'class':'ytd-playlist-video-renderer'})
+
+    singlesong[songName] = songName.find().text.encode('utf-8')
+    allsongs.append(singlesong)
+
+print(allsongs)
+print(youtubebs)
